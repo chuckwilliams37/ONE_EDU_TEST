0486680450e51fdc20eb1bf558eee87211738d24
Chuck <gighak001@gmail.com>
2025-07-08 22:25:08 -0400

    fix: Fixed a module issue

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  webpack: (config, { isServer }) => {
+    // Provide fallbacks for optional ws dependencies
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      bufferutil: false,
+      "utf-8-validate": false,
+    };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
